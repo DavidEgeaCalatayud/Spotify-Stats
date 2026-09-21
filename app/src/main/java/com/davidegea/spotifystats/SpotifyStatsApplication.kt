182860e9e6da10c4cd0608e776b9faf55ec5576d
DavidEgea <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-21 10:57:36 +0200

    feat(import): configure Hilt WorkManager factory

--- app/src/main/java/com/davidegea/spotifystats/SpotifyStatsApplication.kt
+++ app/src/main/java/com/davidegea/spotifystats/SpotifyStatsApplication.kt
@@ -1,7 +1,19 @@
 package com.davidegea.spotifystats
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class SpotifyStatsApplication : Application()
+class SpotifyStatsApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
