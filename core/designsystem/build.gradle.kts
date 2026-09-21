99a1efcfab9cb9a133ec9f6b95df0e59fa9dd565
DavidEgea <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-21 10:18:00 +0200

    design: expose Material navigation icons

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -25,4 +25,5 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
+    api("androidx.compose.material:material-icons-core")
 }
