00680a06b9162d44ab4f4cc26e71c9a3d0c446bf
DavidEgea <114010484+DavidEgeaCalatayud@users.noreply.github.com>
2026-09-21 11:48:25 +0200

    feat(database): upgrade to schema v4 for optional metadata

--- core/database/src/main/java/com/davidegea/spotifystats/database/SpotifyStatsDatabase.kt
+++ core/database/src/main/java/com/davidegea/spotifystats/database/SpotifyStatsDatabase.kt
@@ -10,6 +10,7 @@ import androidx.room.TypeConverters
 import com.davidegea.spotifystats.database.dao.ImportDao
 import com.davidegea.spotifystats.database.dao.ImportJobDao
 import com.davidegea.spotifystats.database.dao.ListeningHistoryDao
+import com.davidegea.spotifystats.database.dao.MetadataDao
 import com.davidegea.spotifystats.database.entity.AlbumEntity
 import com.davidegea.spotifystats.database.entity.ArtistEntity
 import com.davidegea.spotifystats.database.entity.PlayEventEntity
@@ -17,6 +18,8 @@ import com.davidegea.spotifystats.database.entity.TrackArtistCrossRef
 import com.davidegea.spotifystats.database.entity.TrackEntity
 import com.davidegea.spotifystats.database.entity.ImportRunEntity
 import com.davidegea.spotifystats.database.entity.ImportDocumentDiagnosticEntity
+import com.davidegea.spotifystats.database.entity.TrackMetadataEntity
+import com.davidegea.spotifystats.database.entity.AlbumMetadataEntity
 
 @Database(
     entities = [
@@ -30,8 +33,10 @@ import com.davidegea.spotifystats.database.entity.ImportDocumentDiagnosticEntity
         AlbumSearchEntity::class,
         ImportRunEntity::class,
         ImportDocumentDiagnosticEntity::class,
+        TrackMetadataEntity::class,
+        AlbumMetadataEntity::class,
     ],
-    version = 3,
+    version = 4,
     exportSchema = true,
 )
 @TypeConverters(DatabaseConverters::class)
@@ -40,6 +45,7 @@ abstract class SpotifyStatsDatabase : RoomDatabase() {
     abstract fun explorationDao(): ExplorationDao
     abstract fun importDao(): ImportDao
     abstract fun importJobDao(): ImportJobDao
+    abstract fun metadataDao(): MetadataDao
 
     companion object {
         const val DATABASE_NAME = "spotify_stats.db"
